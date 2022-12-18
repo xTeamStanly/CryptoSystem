f561dd0075a0ed163f7bbaceda5dedc25c79b21e
Stamen <xteam.stanly@gmail.com>
2022-12-18 04:31:18 +0100

    enigma rotor + enigma plug board

--- CryptoConsumer/MainForm.cs
+++ CryptoConsumer/MainForm.cs
@@ -19,13 +19,17 @@ namespace CryptoConsumer {
 
         private void MainForm_Load(object sender, EventArgs e) {
 
-            RC4 rc4 = new RC4(new byte[] { 0x52, 0x43, 0x34 });
-            byte[] output = rc4.Encrypt(new byte[] { 0x64, 0x43, 0x6F, 0x64, 0x65 });
+            //RC4 rc4 = new RC4(new byte[] { 0x52, 0x43, 0x34 });
+            //byte[] output = rc4.Encrypt(new byte[] { 0x64, 0x43, 0x6F, 0x64, 0x65 });
 
+
+            //int a = 0;
+
+            // bool validrotor = Utility.valid_rotor_configuration("ABCDEFG         QRSTUVWXYZ");
+
+            var x = Utility.plug_board_configuration_string_to_dictionary("  PO ML IU   KJ NH YT GB VF       RE DC     ");
 
             int a = 0;
-
-
         }
     }
 }
